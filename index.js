5b1333f07e44dc6e7f39157bddefdaa0b4c9f10e
yusufkarakaya <yusufkarakaya92@gmail.com>
2022-05-11 17:05:20 -0700

    code refactoring

--- index.js
+++ index.js
@@ -4,75 +4,68 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-//Show Error Message
+//show error message
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText = message;
+  const errorMessage = formControl.querySelector('small');
+  errorMessage.innerText = message;
 }
-
-//Show Success Massage
+//success error message
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
-
-//Check Input Message
-function checkInput(inputArray) {
+//first character make upper
+function firstFieldUpper(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+//control required
+function isRequired(inputArray) {
+  let isRequired = false;
   inputArray.forEach((input) => {
     if (input.value === '') {
-      showError(input, `${getFirstCharUpper(input)} is required.`);
+      showError(input, `${firstFieldUpper(input)} is required`);
     } else {
-      showSuccess(input);
+      isRequired = true;
     }
+    return isRequired;
   });
 }
 
-//Check Input Length
+//input length control
 function checkLength(input, min, max) {
   if (input.value.length < min) {
     showError(
       input,
-      `${getFirstCharUpper(input)} must be at least ${min} characters`
+      `${firstFieldUpper(input)} must be at least ${min} characters`
     );
   } else if (input.value.length > max) {
     showError(
       input,
-      `${getFirstCharUpper(input)} must be less than ${max} characters`
+      `${firstFieldUpper(input)} must be less than 15 ${max} characters`
     );
   } else {
     showSuccess(input);
   }
 }
 
-function checkEmail(input) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  if (re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, 'Email is not valid');
-  }
-}
-
+//passwords control
 function checkPasswordMatch(input1, input2) {
+  console.log(input1.value);
+  console.log(input2.value);
   if (input1.value !== input2.value) {
-    showError(input2, 'Passwords do not match');
+    showError(input2, 'Password does not match');
+  } else {
+    showSuccess(input2);
   }
 }
 
-function getFirstCharUpper(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  checkInput([username, email, password, password2]);
-  checkLength(username, 3, 12);
+  isRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
   checkLength(password, 6, 25);
-  checkEmail(email);
   checkPasswordMatch(password, password2);
 });
